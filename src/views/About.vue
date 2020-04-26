ab853c9c8dbfb87ff068e6d9b7555d81b2e3cb58
Sulochan Acharya <s.acharya1337@gmail.com>
2020-04-26 01:06:49 -0500

    fixed night switch

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div id="about" :class="{ dark: dark }">
     <div id="nav">
       <NavBar curr="About" />
     </div>
@@ -12,6 +12,30 @@ import NavBar from "@/components/NavBar.vue";
 export default {
   components: {
     NavBar
+  },
+  computed: {
+    dark() {
+      return this.$store.state.isDark;
+    }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+#about {
+  background: url("../assets/home/light-background.png");
+  width: 100vw;
+  height: 100vh;
+}
+
+.dark {
+  background: linear-gradient(to bottom, #323232 0%, #3f3f3f 40%, #1c1c1c 150%),
+    linear-gradient(
+      to top,
+      rgba(255, 255, 255, 0.4) 0%,
+      rgba(0, 0, 0, 0.25) 200%
+    ) !important;
+  background-blend-mode: multiply;
+  color: white;
+}
+</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="contact" :class="{ dark: dark }">
     <div id="nav">
       <NavBar curr="Contact" />
     </div>
@@ -13,9 +13,30 @@ import NavBar from "@/components/NavBar.vue";
 export default {
   components: {
     NavBar
+  },
+  computed: {
+    dark() {
+      return this.$store.state.isDark;
+    }
   }
 };
 </script>
 
 <style>
+#contact {
+  background: url("../assets/home/light-background.png");
+  width: 100vw;
+  height: 100vh;
+}
+
+.dark {
+  background: linear-gradient(to bottom, #323232 0%, #3f3f3f 40%, #1c1c1c 150%),
+    linear-gradient(
+      to top,
+      rgba(255, 255, 255, 0.4) 0%,
+      rgba(0, 0, 0, 0.25) 200%
+    ) !important;
+  background-blend-mode: multiply;
+  color: white;
+}
 </style>
